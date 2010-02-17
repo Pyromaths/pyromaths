9515cb65dbede397aba01c5ef8243f852add01f1
Guillaume barthelemy <guicel@aliceadsl.fr>
2010-02-17 13:41:01 +0100

    mise à jour de l'exercice correction de legers bug d'affichage et possibilité d'ordonnée à l'origine décimale. à la 4e question

--- troisiemes/affine.py
+++ troisiemes/affine.py
@@ -3,7 +3,7 @@
 #
 # Pyromaths
 # Un programme en Python qui permet de créer des fiches d'exercices types de
-# mathématiques niveau collège ainsi que leur corsrigé en LaTeX.
+# mathématiques niveau collège ainsi que leur corrigé en LaTeX.
 # Copyright (C) 2006 -- Jérôme Ortais (jerome.ortais@pyromaths.org)
 #
 # This program is free software; you can redistribute it and/or modify
@@ -24,7 +24,7 @@
 #fonction affine 3e
 import random
 from math import *
-from outils.Affichage import decimaux
+from outils import sepmilliers
 from pyro_classes import Fractions
 
 def extreme(a,b,xmin,xmax,ymin,ymax):
@@ -47,7 +47,7 @@ def extreme(a,b,xmin,xmax,ymin,ymax):
             res.append((xmin,ysort1))
         ysort2=float(y2+coef*(xmax-x2))  #ordonnée du point d'abscisse xmax
         if ysort2 >=ymin and ysort2<=ymax and not(xmax,ysort2) in res:
-            res.append((xmax,ysort2))
+            res.append((xmax,ysort2))   
     else:
         res=[(xmin,y1),(xmax,y1)]
     return res
@@ -64,11 +64,13 @@ def vecdir(A,B):
 def validedroite(A,B):
     #valide le choix du couple A B pour qu'ils ne soient pas "collés",
     # la droite (AB) ne sera ni horizontale ni verticale
-
+    
     rep=True
     if abs(A[0]-B[0])<=1 and abs(A[1]-B[1])<=1:
         rep=False
     if A[0]==B[0] or A[1]==B[1]:
+        rep=False
+    if abs(A[0]-B[0])<1 or abs(A[1]-B[1])<1:
         rep=False
     return rep
 
@@ -79,18 +81,18 @@ def validec(A,B):
     if abs(A[0]-B[0])<=1 and abs(A[1]-B[1])<=1:
         rep=False
     return rep
-
+ 
 def doublefleche(A,B):
     #trace une flèche "double" de justification en pointillés
     mid=(float((A[0]+B[0]))/2,float((A[1]+B[1]))/2)
     res1="\\psline[linestyle=dashed,linewidth=1.1pt]{->}"+str(A)+str(mid)+'\n '
-    res2="\\psline[linestyle=dashed,linewidth=1.1pt]{->}"+str(mid)+str(B)
+    res2="\\psline[linestyle=dashed,linewidth=1.1pt]{->}"+str(mid)+str(B) 
     res=res1+res2
     if A==B:
         res=""
 
     return res
-
+    
 def couple () :
     A=(float(random.randrange(-8,9))/2,float(random.randrange(-8,9))/2)
     B=(float(random.randrange(-8,9))/2,float(random.randrange(-8,9))/2)
@@ -117,12 +119,12 @@ def couples ():
     D=(float(random.randrange(-4,5)),float(random.randrange(-4,5)))
     while not (validec(A,D) and validec(B,D) and validedroite(C,D)) :
         D=(float(random.randrange(-4,5)),float(random.randrange(-4,5)))
-    E=(0,float(random.randrange(-4,5)))
+    E=(0,float(random.randrange(-8,9))/2)
     while not (validec(A,E) and validec(B,E) and validec(C,E) and validec(D,E)):
-        E=(0,float(random.randrange(-4,5)))
+        E=(0,float(random.randrange(-8,9))/2)
     F=(float(random.randrange(-4,5)),float(random.randrange(-4,5)))
     while not (validec(A,F) and validec(B,F) and validec(C,F) and validec(D,F)and validedroite(E,F)):
-        F=(float(random.randrange(-4,5)),float(random.randrange(-4,5)))
+        F=(float(random.randrange(-4,5)),float(random.randrange(-4,5)))    
     return (A,B,C,D,E,F)
 
 def tracedroite(A,B,xmin,xmax,ymin,ymax):
@@ -138,7 +140,7 @@ def dansrep(A,xmin,xmax,ymin,ymax):
     return res
 
 def nomdroite(i,coordo):
-    #place le nom de la droite (d_i) sur le graphique aux coordonnées coordo
+    #place le nom de la droite (d_i) sur le graphique aux coordonnées coordo 
     x0=coordo[0]
     y0=coordo[1]
     if x0!=0:
@@ -170,8 +172,8 @@ def nom3droites(A,B,C,D,E,F,xmin,xmax,ymin,ymax):
         res.append(nomdroite(3,l3[0]))
     else :
         res.append(nomdroite(3,l3[1]))
-    return res
-
+    return res   
+    
 
 
 def isint(x):
@@ -205,39 +207,39 @@ def coefdir(A,B):
     if res[1]<0:
         res=[-res[0],abs(res[1])]
     fr=Fractions.simplifie(Fractions(res[0],res[1]))
-
+    
     return [fr.n,fr.d]
 
 def anteimage(fonc,A,B):
     #Génère la 1ère question et sa réponse
-
-    l=['l\'image de ','un nombre qui a pour image ',u'un antécédent de ']
-    lcor=['l\'image de ',' a pour image ',u'un antécédent de '] #liste pour le corrigé
+    
+    l=['l\'image de ','un nombre qui a pour image ','un ant\\\'ec\\\'edent de ']
+    lcor=['l\'image de ',' a pour image ','un ant\\\'ec\\\'edent de '] #liste pour le corrigé
     i=random.randrange(0,2)
     j=i
     if i==1:
         j=i+random.randrange(0,2)
     res=[]
-    res.append('Donner '+l[j]+decimaux(str(A[i]))+' par la fonction '+'\\textit{'+fonc+'}.')
-    res.append(decimaux(str(A[abs(i-1)]))+' est '+lcor[j]+decimaux(str(A[i]))+' par la \\hbox{fonction '+'\\textit{'+fonc+'}}.')
+    res.append('Donner '+l[j]+sepmilliers(str(A[i]))+' par la fonction '+'\\textit{'+fonc+'}.')
+    res.append(sepmilliers(str(A[abs(i-1)]))+' est '+lcor[j]+sepmilliers(str(A[i]))+' par la \\hbox{fonction '+'\\textit{'+fonc+'}}.')
     if i==0:
         res.append(doublefleche((A[0],0),A))
         res.append(doublefleche(A,(0,A[1])))
-
+        
     else:
-        res.append(doublefleche((0,A[1]),A))
-        res.append(doublefleche(A,(A[0],0)))
+        res.append(doublefleche((0,A[1]),A)) 
+        res.append(doublefleche(A,(A[0],0)))                         
     i=abs(i-1)
     j=i
     if i==1:
         j=i+random.randrange(0,2)
-    res.append('Donner '+l[j]+decimaux(str(B[i]))+' par la fonction '+'\\textit{'+fonc+'}.')
-    res.append(decimaux(str(B[abs(i-1)]))+' est '+lcor[j]+decimaux(str(B[i]))+' par la \\hbox{fonction '+'\\textit{'+fonc+'}}.')
+    res.append('Donner '+l[j]+sepmilliers(str(B[i]))+' par la fonction '+'\\textit{'+fonc+'}.')
+    res.append(sepmilliers(str(B[abs(i-1)]))+' est '+lcor[j]+sepmilliers(str(B[i]))+' par la \\hbox{fonction '+'\\textit{'+fonc+'}}.')
     if i==0:
         res.append(doublefleche((B[0],0),B))
-        res.append(doublefleche(B,(0,B[1])))
+        res.append(doublefleche(B,(0,B[1]))) 
     else:
-        res.append(doublefleche((0,B[1]),B))
+        res.append(doublefleche((0,B[1]),B)) 
         res.append(doublefleche(B,(B[0],0)))
     return res
 
@@ -246,40 +248,40 @@ def tracefonc(f,i,A,B,xmin,xmax,ymin,ymax):
 #Génère la 2e queston et sa réponse
     u=coefdir(A,B)
     if A[1]>=0:
-        b='+'+decimaux(str(A[1]))
+        b='+'+sepmilliers(str(A[1]))
     else:
-        b=decimaux(str(A[1]))
+        b=sepmilliers(str(A[1]))
     if u[1]==1:
-        coef=decimaux(str(u[0]))
+        coef=sepmilliers(str(u[0]))
         if u[0]==-1:
             coef='-'
         if u[0]==1:
             coef=''
-        x1=decimaux(str(B[0]))
-        y1=decimaux(str(B[1]))
+        x1=sepmilliers(str(B[0]))
+        y1=sepmilliers(str(B[1]))
         u[0]=u[0]*B[0]
     else:
         B=(u[1],u[0]+float(A[1]))
         if not dansrep(B,xmin,xmax,ymin,ymax):
             B=(-u[1],-u[0]+float(A[1]))
-
-        x1=decimaux(str(B[0]))
-        y1=decimaux(str(B[1]))
+        
+        x1=sepmilliers(str(B[0]))
+        y1=sepmilliers(str(B[1]))
         if u[0]>0:
-            coef='\\dfrac{'+decimaux(str(u[0]))+'}{'+decimaux(str(u[1]))+'}'
+            coef='\\dfrac{'+sepmilliers(str(u[0]))+'}{'+sepmilliers(str(u[1]))+'}'
         else:
-            coef='-\\dfrac{'+decimaux(str(abs(u[0])))+'}{'+decimaux(str(u[1]))+'}'
-
+            coef='-\\dfrac{'+sepmilliers(str(abs(u[0])))+'}{'+sepmilliers(str(u[1]))+'}'
+            
     x0='0'
     y0=b
     if coef=='' or (coef=='-' and B[0]>0) :
-        st='On sait que $'+f+'(0)='+decimaux(str(A[1]))+'$ et $'+f+'('+x1+')='+coef+x1+b+'='+y1+'$.'
+        st='On sait que $'+f+'(0)='+sepmilliers(str(A[1]))+'$ et $'+f+'('+x1+')='+coef+x1+b+'='+y1+'$.'
     elif coef=='-' and B[0]<0:
-        st='On sait que $'+f+'(0)='+decimaux(str(A[1]))+'$ et $'+f+'('+x1+')='+coef+'('+x1+')'+b+'='+y1+'$.'
-    else:
-        st='On sait que $'+f+'(0)='+decimaux(str(A[1]))+'$ et $'+f+'('+x1+')='+coef+' \\times '+x1+b+'='+decimaux(str(u[0]))+b+'='+y1+'$.'
-
-    l=[u'Tracer la droite représentative ($d_'+str(i)+'$) de la fonction $'+f+':x\\longmapsto '+coef+'x'+b+'$.',
+        st='On sait que $'+f+'(0)='+sepmilliers(str(A[1]))+'$ et $'+f+'('+x1+')='+coef+'('+x1+')'+b+'='+y1+'$.'
+    else:        
+        st='On sait que $'+f+'(0)='+sepmilliers(str(A[1]))+'$ et $'+f+'('+x1+')='+coef+' \\times '+x1+b+'='+sepmilliers(str(u[0]))+b+'='+y1+'$.'
+    
+    l=['Tracer la droite repr\\\'esentative ($d_'+str(i)+'$) de la fonction $'+f+':x\\longmapsto '+coef+'x'+b+'$.',
        st,
        '\\psdot [dotsize=4.5pt,dotstyle=x]'+str(A),
        '\\psdot [dotsize=4.5pt,dotstyle=x]'+str(B),
@@ -292,68 +294,76 @@ def exprfonc(f,i,A,B):
 #A est sur l'axe des ordonnées, f est le nom de la fonction
     u=coefdir(A,B)
     if A[1]>=0:
-        b='+'+decimaux(str(A[1]))
+        b='+'+sepmilliers(str(A[1]))
     else:
-        b=decimaux(str(A[1]))
+        b=sepmilliers(str(A[1]))
     if u[1]==1:
-        coef=decimaux(str(u[0]))
+        coef=sepmilliers(str(u[0]))
         if u[0]==-1:
             coef='-' #utilisé dans l'expression de la fonction
         if u[0]==1:
             coef=''
-        coefres=decimaux(str(u[0]))  #résultat utilisé pour a
+        coefres=sepmilliers(str(u[0]))  #résultat utilisé pour a
     else:
         if u[0]>0:
-            coef='\\dfrac{'+decimaux(str(u[0]))+'}{'+decimaux(str(u[1]))+'}'
+            coef='\\dfrac{'+sepmilliers(str(u[0]))+'}{'+sepmilliers(str(u[1]))+'}'
         else:
-            coef='-\\dfrac{'+decimaux(str(abs(u[0])))+'}{'+decimaux(str(u[1]))+'}'
+            coef='-\\dfrac{'+sepmilliers(str(abs(u[0])))+'}{'+sepmilliers(str(u[1]))+'}'
         coefres=coef
 
     if A[1]-B[1]>0:
-        deltay='+'+decimaux(str(A[1]-B[1]))
+        deltay='+'+sepmilliers(str(A[1]-B[1]))
     else :
-        deltay=decimaux(str(A[1]-B[1]))
+        deltay=sepmilliers(str(A[1]-B[1]))
     if A[0]-B[0]>0:
-        deltax='+'+decimaux(str(A[0]-B[0]))
+        deltax='+'+sepmilliers(str(A[0]-B[0]))
     else:
-        deltax=decimaux(str(A[0]-B[0]))
-
+        deltax=sepmilliers(str(A[0]-B[0]))
+         
     if float(B[0])<0 :
-       mid11=float(B[0])-0.6
+       mid11=float(B[0])-0.75
        mid12=float((B[1]+A[1]))/2   #milieu de la flèche verticale
     else:
-        mid11=float(B[0])+0.6
+        mid11=float(B[0])+0.75
         mid12=float((B[1]+A[1]))/2
     if float(B[0])*float(u[1]/u[0])>0 :
         mid21=float((A[0]+B[0]))/2
         mid22=A[1]-0.6               #milieu de la flèche horizontale
     else :
         mid21=float((A[0]+B[0]))/2
-        mid22=A[1]+0.6
+        mid22=A[1]+0.5
     if mid12 < 0 and mid12 >-0.8:
         mid12=-1
-    if mid21 < 0 and mid21 >-0.8:
-        mid21=-1
     if mid12 >= 0 and mid12 < 0.5:
         mid12=0.5
-    if mid21 >-0.8 and mid21 < 0:
-        mid21=-0.8
+    if mid11 < 0 and mid11 >-0.8:
+        mid11=-1    
+    if mid11 >=0 and mid11 < 0.5:
+        mid11=0.5
+    if mid21 < 0 and mid21 >-0.8:
+        mid21=-1    
+    if mid21 >=0 and mid21 < 0.5:
+        mid21=0.5
+    if mid22 < 0 and mid22 >-0.8:
+        mid22=-1
+    if mid22 >= 0 and mid22 < 0.5:
+        mid22=0.5
     mid1=(mid11,mid12)
     mid2=(mid21,mid22)
-
-    l=[u'Déterminer l\'expression de la fonction $'+f+u'$ représentée ci-contre par la droite ($d_'+str(i)+'$).',
-       u'On lit l\'ordonnée à l\'origine et le coefficient de la fonction affine sur le graphique.\\\ ',
-       '$'+f+'(x)=ax+b$ ' +'avec $b='+ decimaux(str(A[1]))+'$ et $a='+'\\dfrac{'+deltay+'}{'+deltax+'}='+coefres+'$.\\\ ',
-       'L\'expression de la fonction '+f+' est $'+f+'(x)='+coef+'x'+b+'$.',
+       
+    l=['D\\\'eterminer l\'expression de la fonction $'+f+'$ repr\\\'esent\\\'ee ci-contre par la droite ($d_'+str(i)+'$).',
+       'On lit l\'ordonn\\\'e \`a l\'origine et le coefficient de la fonction affine sur le graphique.\\\ ',
+       '$'+f+'(x)=ax+b$ ' +'avec $b='+ sepmilliers(str(A[1]))+'$ et $a='+'\\dfrac{'+deltay+'}{'+deltax+'}='+coefres+'$.\\\ ',
+       'L\'expression de la fonction $'+f+'$ est $'+f+'(x)='+coef+'x'+b+'$.',
        doublefleche(B,(B[0],A[1])),
        doublefleche((B[0],A[1]),A),
        '\\rput'+str(mid1)+'{('+deltay+')}',
        '\\rput'+str(mid2)+'{('+deltax+')}']
-
+  
     return l
 
 
-def affine(exo,cor):
+def affine(f0,f1):
     #Génère l'exercice
     xmin,xmax,ymin,ymax=-5,5,-5,5
     f=['f','g','h','k','l','u']
@@ -366,8 +376,8 @@ def affine(exo,cor):
     fonc3=f[(rgfonc1+2)%6]
     l3=exprfonc(fonc3,3,E,F)
     noms=nom3droites(A,B,C,D,E,F,xmin,xmax,ymin,ymax)
-    exos=["\\parbox{0.5\\linewidth}{",
-         u"($d_1$) est la droite représentative de la fonction $"+fonc1+"$.",
+    exo=["\\parbox{0.5\\linewidth}{",
+         "($d_1$) est la droite repr\\'esentative de la fonction $"+fonc1+"$.",
          "\\begin{enumerate}",
          "\\item "+l[0],
          "\\item "+l[4],
@@ -386,22 +396,16 @@ def affine(exo,cor):
          noms[2],
          "\\end{pspicture}}"]
 
-    cors=["\\setlength{\\columnsep}{2mm}",
+    cor=["\\setlength{\\columnsep}{2mm}",
          "\\begin{multicols}{2}\\noindent \\small",
-         u"($d_1$) est la droite représentative de la fonction $"+fonc1+"$.",
+         "($d_1$) est la droite repr\\'esentative de la fonction $"+fonc1+"$.",
          "\\begin{enumerate}",
          "\\item "+l[1],
          "\\item "+l[5],
-         "\\item",
-         "\\begin{flushleft}",
-         l2[1],
-         " \\end{flushleft}",
-         "\\item",
-         "\\begin{flushleft}",
-         l3[1],
+         "\\item \\begin{flushleft}"+l2[1]+" \\end{flushleft}",
+         "\\item \\begin{flushleft}"+l3[1],
          l3[2],
-         l3[3],
-         "\\end{flushleft}",
+         l3[3]+"\\end{flushleft}",
          "\\end{enumerate}",
          "\\end{multicols}",
          "\\vspace{0.45cm}",
@@ -442,13 +446,14 @@ def affine(exo,cor):
          "\\end{center}",
          "\\end{minipage}",
          "\\vspace{0.45cm}"]
-    for st in exos:
-        exo.append(st+'\n')
-    for st in cors:
-        cor.append(st+'\n')
+    for st in exo:
+        f0.write(st+'\n')
+    for st in cor:
+        f1.write(st+'\n')
+    
+         
 
-def tex_affine():
-    exo = ['\\exercice\n']
-    cor = ['\\exercice*\n']
-    affine(exo,cor)
-    return (exo, cor)
+
+
+    
+
