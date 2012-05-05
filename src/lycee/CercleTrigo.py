f6d17feac0c24e7578a694e0486e4d1c88e80a51
Arnaud Kientz <arnaud@pyromaths.org>
2012-05-05 23:35:20 +0200

    Correction de l'exercice sur le cercle trigonométrique.

--- src/lycee/CercleTrigo.py
+++ src/lycee/CercleTrigo.py
@@ -41,7 +41,7 @@ def simprad(liste):
 
 def rad2deg(liste):
     """Effectue la conversion de radians entre 0 et 2pi vers degrés."""
-    return liste[0]*180/liste[1]
+    return liste[0]*180/liste[1] # Attention résultat entier, donc faux si liste[1] ne divise pas 180
 
 def rad2tex(liste):
     """Prépare l'écriture de radians en latex."""
@@ -68,7 +68,7 @@ def mes_princ(liste):
     num = liste[0] % (2*liste[1])
     if num > liste[1]:
         num -= 2*liste[1]
-    return [num,liste[1]] 
+    return simprad([num,liste[1]])
 
 def mes_02pi(liste):
     """Calcule la mesure d'un angle en radians dans l'intervalle [0,2pi]."""
@@ -80,7 +80,7 @@ def cercle_trigo(code=0,*args):
     figure = """\\psset{xunit=3cm,yunit=3cm,dotstyle=+}
     \\begin{pspicture}(-2,-2)(2,2)
     \\psframe(-2,-2)(2,2)
-    \\psaxes[linewidth=0.5pt,Dx=2,Dy=2]{->}(0,0)(-1.25,-1.25)(1.25,1.25)
+    \\psaxes[linewidth=0.5pt,Dx=2,Dy=2]{->}(0,0)(-1.1,-1.1)(1.1,1.1)
     \\pscircle[linecolor=green](0,0){3}\\n"""
     idx = 0
     for f in args:
@@ -93,9 +93,9 @@ def cercle_trigo(code=0,*args):
             
         figure += "\\psline[linecolor=blue,linestyle=dashed](0,0)("+str(round(cos(f),2))+","+str(round(sin(f),2))+")\n"
         if code:
-            figure += "\\rput("+str(round(1.1*cos(f),2))+","+str(round(1.1*sin(f),2))+"){"+frac+"}\n"
+            figure += "\\rput("+str(round(1.25*cos(f),2))+","+str(round(1.25*sin(f),2))+"){"+frac+"}\n"
         else:
-            figure += "\\rput("+str(round(1.1*cos(f),2))+","+str(round(1.1*sin(f),2))+"){$M_"+str(idx)+"$}\n"
+            figure += "\\rput("+str(round(1.25*cos(f),2))+","+str(round(1.25*sin(f),2))+"){$M_"+str(idx)+"$}\n"
             idx += 1
     figure += """\\uput[dl](0,0){$O$}
     \uput[dr](1,0){$I$}
@@ -107,12 +107,18 @@ def findrad(itv=1, neg=0, denom_simple=1):
     """Génère un angle en radians sous certaines conditions :
     itv ( intervalle ): 0 ( quelconque ), 1 ( mesure principale ), 2 ( entre 0 et 2 pi )
     neg : 0 ( positif ), 1 ( négatif )
-    denom_simple : 0 ( quelconque ), 1 ( simple, càd 1, 2, 3, 4, 5, ou 6 )"""
+    denom_simple : 0 ( quelconque ), 1 ( simple, càd 1, 2, 3, 4, 5, ou 6 ), 2 ( diviseur de 180 )"""
     
-    if denom_simple:
+    global div180
+    
+    if denom_simple==1:
         denom = random.randint(2,6)
-    else:
+    elif denom_simple == 0:
         denom = random.randint(2,30) 
+    else:
+        random.shuffle(div180)
+        random.shuffle(div180[0])
+        denom = div180[0][0]
     
     if itv == 1:
         sgn = (-1)**(random.randint(1,2))
@@ -135,12 +141,55 @@ def cercle_trigonometrique():
     exo = ["\\exercice"]
     cor = ["\\exercice*"]  
     
-    mes_deg = [random.randint(0,360) for f in range(5)] # FIX à revoir, trop complexes, traitement des doublons ?
+    mes_deg = [] # Question 1
+    while len(mes_deg)<5:
+        tmp = random.randint(0,360) # FIX trop complexes parfois
+        if tmp not in mes_deg:
+            mes_deg.append(tmp) 
     mes_deg_tex = [u"$%s\\degres$" %(d) for d in mes_deg]
-    radq2 = [findrad(2,0,0) for f in range(5)]
-    radq3 = [findrad(0,0,0) for f in range(4)] + [findrad(0,1,0)] # dernier négatif
-    radq4 = [findrad(0,0,1) for f in range(4)]
-    radq5 = [findrad(1,0,1) for f in range(2)] + [findrad(1,1,1)] + [findrad(0,0,1)] # avant-dernier négatif et dernier non principal
+    
+    radq2 = [] # Question 2
+    while len(radq2)<5:
+        tmp = findrad(2,0,2) # Dénominateur est un diviseur de 180
+        if tmp not in radq2:
+            radq2.append(tmp)
+           
+    radq3 = [] # Question 3
+    while len(radq3)<5:
+        while len(radq3)<4:
+            tmp = findrad(0,0,0)
+            if tmp not in radq3:
+                radq3.append(tmp)        
+        tmp = findrad(0,1,0) # dernier négatif
+        if tmp not in radq3:
+            radq3.append(tmp)    
+    
+    radq4 = [] # Question 4
+    while len(radq4)<4:
+        tmp = mes_princ(findrad(0,0,1))
+        if tmp not in radq4:
+            radq4.append(tmp)    
+    
+    radq5 = [] # Question 5
+    radq5_princ = []
+    while len(radq5)<4:
+        while len(radq5)<2:
+            tmp = findrad(1,0,1)
+            tmp_princ = mes_princ(tmp)
+            if tmp_princ not in radq5_princ:
+                radq5.append(tmp)
+                radq5_princ.append(tmp_princ)
+        while len(radq5)<3:
+            tmp = findrad(1,1,1) # avant-dernier négatif
+            tmp_princ = mes_princ(tmp)
+            if tmp_princ not in radq5_princ:
+                radq5.append(tmp)  
+                radq5_princ.append(tmp_princ)
+        tmp = findrad(0,0,1) # dernier non principal
+        tmp_princ = mes_princ(tmp)
+        if tmp_princ not in radq5_princ:
+            radq5.append(tmp)    
+            
     mes_rad = radq2 + radq3 + radq4 + radq5
     mes_rad_tex = [rad2tex(r)for r in mes_rad]
     
@@ -149,11 +198,11 @@ def cercle_trigonometrique():
     
     exo.append(u"\\begin{enumerate}\\\\\\n")
     exo.append(u"\\item Convertir les cinq mesures suivantes en radians : %s, %s, %s, %s et %s.\\\\\\n" %(tuple(mes_deg_tex)))
-    exo.append(u"\\item Convertir les cinq mesures suivantes en degrés : %s, %s, %s, %s et %s.\\\\\\n" %(tuple(mes_rad_tex[0:5])))
-    exo.append(u"\\item Déterminer les mesures principales des angles suivants en radians : %s, %s, %s, %s et %s.\\\\\\n" %(tuple(mes_rad_tex[5:10])))
-    exo.append(u"\\item Des angles ont été placés sur le cercle trigonométrique ci-dessous, lire leurs mesures principales en radians.\\\\\\n")
+    exo.append(u"\\item Convertir les cinq mesures suivantes en degrés : %s, %s, %s, %s et %s rad.\\\\\\n" %(tuple(mes_rad_tex[0:5])))
+    exo.append(u"\\item Déterminer les mesures principales des angles suivants en radians : %s, %s, %s, %s et %s rad.\\\\\\n" %(tuple(mes_rad_tex[5:10])))
+    exo.append(u"\\item Des angles ont été placés sur le cercle trigonométrique ci-dessous, représentés par les points $M_0$, $M_1$, $M_2$ et $M_3$. Lire leurs mesures principales en radians.\\\\\\n")
     exo.append(cercle_trigo(0,radq4[0],radq4[1],radq4[2],radq4[3]))
-    exo.append(u"\\item Placer les angles suivants sur le cercle trigonométrique : %s, %s, %s et %s.\\\\\\n" %(tuple(mes_rad_tex[14:])))
+    exo.append(u"\\item Placer les angles suivants sur le cercle trigonométrique : %s, %s, %s et %s rad.\\\\\\n" %(tuple(mes_rad_tex[14:])))
     exo.append(cercle_trigo())
     exo.append(u"\\end{enumerate}\\\\\\n")
     
@@ -164,12 +213,12 @@ def cercle_trigonometrique():
     cor.append(u"Par exemple pour la première mesure, on obtient avec simplification : $"+str(mes_deg[0])+"\\times\\dfrac{\\pi}{180}$ = "+mes_deg_en_rad_tex[0]+"rad.\\\\\\n")
     cor.append(u"De même pour les autres mesures, on trouve alors respectivement : %s rad, %s rad, %s rad, %s rad et %s rad.\\\\\\n" %(tuple(mes_deg_en_rad_tex)))
     
-    cor.append(u"\\item Convertir les cinq mesures suivantes en degrés : %s, %s, %s, %s et %s.\\\\\\n" %(tuple(mes_rad_tex[0:5])))
+    cor.append(u"\\item Convertir les cinq mesures suivantes en degrés : %s, %s, %s, %s et %s rad.\\\\\\n" %(tuple(mes_rad_tex[0:5])))
     
     cor.append(u"On effectue alors la proportionalité inverse : il faut multiplier par $\\dfrac{180}{\\pi}$.\\\\\\n")
     cor.append(u"Après simplification, voici les résultats : %s \\degres, %s \\degres, %s \\degres, %s \\degres et %s \\degres.\\\\\\n" %(tuple([rad2deg(r) for r in radq2])))
     
-    cor.append(u"\\item Déterminer les mesures principales des angles suivants en radians : %s, %s, %s, %s et %s.\\\\\\n" %(tuple(mes_rad_tex[5:10])))
+    cor.append(u"\\item Déterminer les mesures principales des angles suivants en radians : %s, %s, %s, %s et %s rad.\\\\\\n" %(tuple(mes_rad_tex[5:10])))
     
     rep = mes_princ(radq3[0])
     nb_tours = (radq3[0][0]-rep[0])/(2*radq3[0][1])
@@ -179,16 +228,17 @@ def cercle_trigonometrique():
     cor.append(rad2tex(radq3[0])[:-1]+"\\equiv"+rad2tex(rep)[1:-1]+"+"+rad2tex([2*nb_tours*rep[1],rep[1]])[1:-1]+"\\equiv"+rad2tex(rep)[1:-1]+"+"+rad2tex([2*nb_tours,1])[1:-1]+"\\equiv"+rad2tex(rep)[1:-1]+u"(2\\pi)$.\\\\\\n")
     cor.append(u"De même pour les autres mesures, on trouve alors respectivement : %s rad, %s rad, %s rad, %s rad et %s rad.\\\\\\n" %(tuple([rad2tex(mes_princ(r)) for r in radq3])))
     
-    cor.append(u"\\item Des angles ont été placés sur le cercle trigonométrique ci-dessous, lire leurs mesures principales en radians.\\\\\\n")
+    cor.append(u"\\item Des angles ont été placés sur le cercle trigonométrique ci-dessous, représentés par les points $M_0$, $M_1$, $M_2$ et $M_3$. Lire leurs mesures principales en radians.\\\\\\n")
     
     cor.append(u"Les réponses sont directement données sur le cercle trigonométrique ci-dessous :\\\\\\n")
     cor.append(cercle_trigo(1,mes_princ(radq4[0]),mes_princ(radq4[1]),mes_princ(radq4[2]),mes_princ(radq4[3])))
     
-    cor.append(u"\\item Placer les angles suivants sur le cercle trigonométrique : %s, %s, %s et %s.\\\\\\n" %(tuple(mes_rad_tex[14:])))
+    cor.append(u"\\item Placer les angles suivants sur le cercle trigonométrique : %s, %s, %s et %s rad.\\\\\\n" %(tuple(mes_rad_tex[14:])))
     
     cor.append(u"Les réponses sont directement données sur le cercle trigonométrique ci-dessous :\\\\\\n")
     cor.append(cercle_trigo(1,radq5[0],radq5[1],radq5[2],radq5[3]))
-
+    cor.append(u"Ajoutons une simple remarque pour la dernière mesure, qui n'est pas principale : il faut effectuer en premier lieu une simplification, comme à la question 3. On obtient alors :\\\\\\n")
+    cor.append(rad2tex(radq5[3])[:-1]+"\\equiv"+rad2tex(mes_princ(radq5[3]))[1:-1]+" (2\\pi)$.")
     cor.append(u"\\end{enumerate}\\\\\\n") 
     
     return exo, cor
