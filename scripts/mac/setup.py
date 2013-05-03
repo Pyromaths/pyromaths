606a631893b7437921b9a947fde5cab70239d3ef
Yves Gesnel <yves@gesnel.fr>
2013-05-03 19:11:16 +0200

    Version Mac: intégration à setup.py des informations de Info.plist (qui est supprimé)

--- scripts/mac/setup.py
+++ scripts/mac/setup.py
@@ -1,11 +1,30 @@
+# -*- coding: utf-8 -*-
 from glob import glob
 from setuptools import setup, find_packages
+
+import sys
+sys.path.append('../../src/pyromaths/')
+from Values import VERSION
+
+plist = dict(
+        CFBundleIdentifier = "org.pyromaths.pyromaths",
+        CFBundleName = "Pyromaths",
+        CFBundlePackageType = "APPL",
+        CFBundleShortVersionString = "%s" % VERSION,
+        CFBundleVersion = "%s" % VERSION,
+        NSHumanReadableCopyright = u"© Jérôme Ortais",
+        CFBundleIconFile = "pyromaths",
+        CFBundleDevelopmentRegion = "French",
+        CFBundleExecutable = "pyromaths",
+        CFBundleDisplayName = "pyromaths",
+        CFBundleSignature = "PYTS"
+        )
 
 qt_unused = ['QtDBus', 'QtDeclarative', 'QtDesigner', 'QtHelp', 'QtMultimedia',
              'QtNetwork', 'QtOpenGL', 'QtScript', 'QtScriptTools', 'QtSql',
              'QtSvg', 'QtTest', 'QtWebKit', 'QtXml', 'QtXmlPatterns', 'phonon']
 OPTIONS = dict(
-        plist='Info.plist',
+        plist=plist,
         argv_emulation=True,
         iconfile='pyromaths.icns',
         includes=['gzip'],
