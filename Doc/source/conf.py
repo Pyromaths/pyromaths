1509ed0063403cc86b7a0e0c05911b2732c62514
Louis <spalax@gresille.org>
2018-05-13 14:28:40 +0200

    [doc] Chargement de gettext (pour éviter une erreur à la compilation de la documentation)

--- Doc/source/conf.py
+++ Doc/source/conf.py
@@ -14,11 +14,17 @@
 import sys, os, locale
 locale.setlocale(locale.LC_TIME,'')
 
+
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 sys.path.insert(0, os.path.abspath('../../src/'))
+
+import gettext
+gettext.install('pyromaths', unicode=1)
+
 from pyromaths.Values import VERSION, COPYRIGHT_YEAR
+
 # -- General configuration -----------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
