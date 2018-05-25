0877d049041855c017867cc2506edd8cd197aa90
Louis <spalax@gresille.org>
2018-05-25 18:31:56 +0200

    Mise à jour de `utils/cree-vignettes`
    * Le script re-générait *toutes* les vignettes à chaque fois. Ce n'est
      plus le cas.
    * Le script supposait encore (en partie) que les exercices s'appelaient
      `exercice-NUMERO` (c'est sans doute la raison du bug précédent).
    * Le script ne fait plus d'appel à git (c'est moins nécessaire, puisque
      les fichiers ne changent plus de nom).

--- utils/creer-vignettes.py
+++ utils/creer-vignettes.py
@@ -1,130 +1,140 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-import os, sys, codecs, shutil, glob
-from os.path import dirname, normpath, join, abspath, realpath, split
-from subprocess import call, Popen
 
-_path = normpath(join(abspath(dirname(__file__)), "../src"))
-sys.path[0] = realpath(_path)
-import hashlib
+# pylint: disable=invalid-name
+
+"""Crée et met à jour les vignettes des exercices."""
+
 import gettext
+import hashlib
+import json
+import logging
+import os
+import random
+import shutil
+import sys
+import tempfile
+from subprocess import call
+from contextlib import contextmanager
 
-def main():
-#===============================================================================
-# Imports spécifiques à Pyromaths
-#===============================================================================
+# Définition de `_()` comme la fonction identité.
+# Pour le moment, les vignettes des exercices ne sont pas traduites.
+gettext.install('pyromaths', unicode=1)
 
-    locale_dir = join(dirname(__file__), '../locale/')
-    locale_dir = realpath(locale_dir)
+ROOTDIR = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
+PYROMATHSPATH = os.path.join(ROOTDIR, "src")
 
-    gettext.install('pyromaths', localedir=locale_dir, unicode=1)
-    from pyromaths import pyromaths, ex
-    from pyromaths.Values import data_dir, configdir
-    from pyromaths.outils.System import creation
-    _param = {
+sys.path.insert(0, os.path.realpath(PYROMATHSPATH))
+# pylint: disable=wrong-import-position
+import pyromaths
+from pyromaths.Values import data_dir, configdir
+from pyromaths.outils.System import creation
+
+THUMBDIR = os.path.join(data_dir(), "ex", "img")
+MD5PATH = os.path.join(THUMBDIR, "md5sum.json")
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s %(message)s',
+    )
+
+@contextmanager
+def md5file():
+    """Contexte pour lire et écrire les md5sum des exercices."""
+    # Création du fichier s'il n'existe pas déjà.
+    if not os.path.exists(MD5PATH):
+        with open(MD5PATH, mode="w") as fichier:
+            json.dump({}, fichier)
+
+    logging.info("Lecture du fichier '%s'.", MD5PATH)
+    with open(MD5PATH, mode="r") as fichier:
+        md5sums = json.loads(fichier.read())
+
+    yield md5sums
+
+    logging.info("Écriture du fichier '%s'.", MD5PATH)
+    with open(MD5PATH, mode="w") as fichier:
+        json.dump(md5sums, fichier, sort_keys=True, indent=4)
+
+@contextmanager
+def create_temp_dir():
+    """Contexte pour la création et la suppression du répertoire temporaire."""
+    name = tempfile.mkdtemp()
+
+    yield name
+
+    shutil.rmtree(name)
+
+def parametres_exo(exo, tempdir):
+    """Renvoit les paramètres de la création d'un unique exercice."""
+    return {
         'creer_pdf': True,
         'creer_unpdf': False,
         'titre': u"Thumbnail",
         'corrige': False,
         'niveau': "",
         'nom_fichier': u'thumb.tex',
-        'chemin_fichier': "/tmp/",
-        'fiche_exo': '/tmp/thumb.tex',
-        'fiche_cor': '/tmp/thumb-corrige.tex',
+        'chemin_fichier': tempdir,
+        'fiche_exo': os.path.join(tempdir, 'thumb.tex'),
+        'fiche_cor': os.path.join(tempdir, 'thumb-corrige.tex'),
         'configdir': configdir(),
         'datadir': data_dir(),
         'modele': 'pyromaths.tex',
-        'liste_exos': [],
         'openpdf': 0,
+        'liste_exos': [exo()],
     }
 
 
-    def md5check(exercise, imgdir, newfile=None):
-        ''' Check if the file has been changed or renamed. '''
-        if newfile is None:
-            newfile = os.path.join(imgdir,'tmp', os.path.basename(exercise.thumb()))
-        # open md5sums file
-        md = open(os.path.join(imgdir, 'md5sums'), 'r')
-        thesum = hashlib.md5(open(newfile, 'rb').read()).hexdigest()
-        print thesum
-        originalfile=''
-        newname=True
-        for l in md:
-            if thesum in l:
-                originalfile=os.path.join(imgdir, l.lstrip(thesum).lstrip())
-            if os.path.basename(newfile) in l:
-                newname=False
-        md.close()
-        shutil.copyfile(newfile, exercise.thumb())
-        os.chdir(imgdir)
-        #raise ValueError('asked to stop')
-        if originalfile:
-            if originalfile!=exercise.thumb():
-                log.write('%s renamed to %s' % (originalfile, exercise.thumb()))
-                call(['git', 'mv', originalfile, exercise.thumb()], stdout=log)
-        else:
-            if newname:
-                log.write('Created file %s' % (exercise.thumb()))
-                call(['git', 'add', exercise.thumb()], stdout=log)
-            else:
-                log.write('%s replaced by %s' % (originalfile, exercise.thumb()))
-        os.remove(os.path.join(newfile))
+def create_thumbnail(exercise, tempdir):
+    """Crée la vignette de l'exercice"""
+    outfile = exercise.thumb()
 
-    def _thumb(exercise, outfile=None):
-        ''' Create a thumbnail for this 'exercise'. '''
-        if outfile is None:
-            print("EXERCICE.THUMB : ",exercise.thumb())
-            outfile = os.path.join(os.path.dirname(exercise.thumb()), 'tmp', os.path.basename(exercise.thumb()))
-        print outfile
-        # create pdf
-        _param['liste_exos'] = [exercise()]
-        creation(_param)
-        # extract optimized thumbnail
-        call(["convert", "-density", "288", "/tmp/thumb.pdf",
-            "-resize", "25%", "-crop", "710x560+0+85",
-            "-flatten", "-trim", "/tmp/thumb.png"],
-            stdout=log)
-        call(["pngnq", "-f", "-s1", "-n32", "/tmp/thumb.png"], stdout=log)
-        log.write(outfile)
-        shutil.copyfile( "/tmp/thumb-nq8.png", outfile)
-        call(args=["optipng", "-o7", outfile])
-        md5check(exercise, os.path.dirname(exercise.thumb()))
+    logging.info("Compilation de l'exercice.")
+    random.seed(0)
+    creation(parametres_exo(exercise, tempdir))
 
-    def thumbs(pkg=ex, recursive=True):
-        ''' Create all exercise thumbnails. '''
-        import random
-        random.seed(0)
-        imgdir = join(_param['datadir'], 'ex', 'img')
-        print "IMGDIR : ", imgdir
-        try:
-            os.mkdir(os.path.join(imgdir, 'tmp'))
-        except OSError:
-            pass
-        #raise ValueError('voila')
-        for fl in glob.glob(join(imgdir, "ex-??.png")):
-            #Do what you want with the file
-            os.remove(fl)
-        for e in ex._exercises(pkg):
-            _thumb(e)
-        if not recursive: return
-        try:
-            os.chdir(imgdir)
-            mdfile=open('md5sums','w')
-            for fl in glob.glob(join(imgdir, "ex-??.png")):
-                #Do what you want with the file
-                thesum = hashlib.md5(open(fl, 'rb').read()).hexdigest()
-                mdfile.write(thesum + '  ' + os.path.basename(fl) + '\n')
-            mdfile.close()
-            os.rmdir(os.path.join(imgdir, 'tmp'))
-        except OSError:
-            pass
-        for pk in ex._subpackages(pkg):
-            thumbs(pk)
+    logging.info("Extraction de la vignette.")
+    call([
+        "convert",
+        "-density", "288",
+        os.path.join(tempdir, "thumb.pdf"),
+        "-resize", "25%",
+        "-crop", "710x560+0+85",
+        "-flatten", "-trim",
+        os.path.join(tempdir, "thumb.png"),
+        ])
 
-    log = open('/tmp/preview-pyromaths.log' , 'w')
-    thumbs()
-    log.close()
+    logging.info("Appel de `pngnq` sur la vignette.")
+    call([
+        "pngnq", "-f", "-s1", "-n32", os.path.join(tempdir, "thumb.png"),
+        ])
+    shutil.copyfile(os.path.join(tempdir, "thumb-nq8.png"), outfile)
 
+    logging.info("Optimisation de la vignette.")
+    call(args=["optipng", "-o7", outfile])
+
+def md5sum(exo):
+    """Calcule et renvoit le hash md5sum de l'énoncé 0 de l'exercice."""
+    random.seed(0)
+    return hashlib.md5(
+        "\n"
+        .join(exo().tex_statement())
+        .encode(errors="backslashreplace")
+        ).hexdigest()
+
+def main(tempdir):
+    """Fonction principale"""
+    with md5file() as md5sums:
+        levels = pyromaths.ex.load_levels()
+        for level in levels:
+            for exo in levels[level]:
+                logging.info("Exercice '%s'.", exo.name())
+                if md5sums.get(exo.name(), "0") == md5sum(exo):
+                    logging.info("L'exercice n'a pas été modifié.")
+                    continue
+                create_thumbnail(exo, tempdir)
+                md5sums[exo.name()] = md5sum(exo)
 
 if __name__ == "__main__":
-    main()
+    with create_temp_dir() as temp:
+        main(temp)
